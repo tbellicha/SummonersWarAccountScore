12619ad73835bf20903f272b9abb6ce004a121a9
BELLICHA Tanguy <tanguy.bellicha@epitech.eu>
2022-06-28 13:47:50 +0200

    [ADD] std before some system call

--- src/utils/getMemberValue.cpp
+++ src/utils/getMemberValue.cpp
@@ -14,7 +14,7 @@ std::string getMemberValue(std::ifstream &file, std::string member)
     std::string complete_member = "\"" + member + "\": ";
     std::string line;
 
-    while (getline(file, line))
+    while (std::getline(file, line))
         if (line.find(complete_member) != std::string::npos)
             return line.substr(line.find(complete_member) + complete_member.length(), line.length() - 1 - (line.find(complete_member) + complete_member.length()));
     return "";
